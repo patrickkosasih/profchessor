9682e1523581c5a6985c4ded44d0ad154b816511
patrickkosasih <77109385+patrickkosasih@users.noreply.github.com>
2023-03-17 16:43:29 +0700

    Piece test (fail)
    The pieces doesn't have a transparent background and the drag and drop mechanism makes the piece teleport back and forth for some reason. Planning on switching to a canvas.

--- gui.py
+++ gui.py
@@ -1,21 +1,62 @@
 import tkinter as tk
+from PIL import Image, ImageTk
+import os
 
 import shared
 
 
-class Piece(tk.Label):  # Still uses letters to represent the piece type
-    def __init__(self, master, piece_type, **kwargs):
-        super().__init__(master, **kwargs)
+class Piece(tk.Label):
+    def __init__(self, board, piece_type, **kw):
+        super().__init__(board, **kw)
 
         if piece_type not in shared.PIECE_TYPES:
             raise ValueError(f"invalid piece type \"{piece_type}\"")
 
-        self.configure(text=piece_type, font=("Arial", 24))
+        # Mouse related variables
+        self.dragging = False  # True if piece is being dragged by the mouse
+        self.position_before = (0, 0)  # Position before being dragged
+
+        # Load image
+        piece_size = board.board_size // 8
+        image = Image.open("sprites/black pawn.png").resize((piece_size, piece_size))
+        self.sprite = ImageTk.PhotoImage(image)
+
+        self.configure(image=self.sprite)
+
+        self.bind("<Button-1>", self.on_click)
+        self.bind("<ButtonRelease-1>", self.on_release)
+        self.bind("<B1-Motion>", self.on_move)
+
+    def place_center(self, x, y, **kw):
+        # Calculate the top left coordinate
+        tl_x = x - self.winfo_width() // 2
+        tl_y = y - self.winfo_height() // 2
+
+        self.place(x=tl_x, y=tl_y, **kw)
+
+    def on_click(self, event):
+        self.dragging = True
+        self.position_before = (self.winfo_rootx(), self.winfo_rooty())
+
+        print("click", event.x, event.y)
+
+        x, y = event.x, event.y
+        self.position_before = x, y
+        self.place_center(x=x, y=y)
+
+    def on_release(self, event):
+        print("release")
+        self.dragging = False
+
+    def on_move(self, event):
+        x, y = event.x, event.y
+
+        self.place_center(x=x, y=y)
 
 
 class Square(tk.Frame):
-    def __init__(self, master, i, size, **kw):
-        super().__init__(master, **kw)
+    def __init__(self, board, i, size, **kw):
+        super().__init__(board, **kw)
 
         self.i = i
         self.coordinate = chr(ord("a") + i % 8) + str(8 - i // 8)
@@ -35,12 +76,16 @@ class Square(tk.Frame):
 
 
 class Board(tk.Frame):
-    def __init__(self, master, size, **kw):
-        super(Board, self).__init__(master, width=size, height=size, **kw)
+    def __init__(self, root, size, **kw):
+        super(Board, self).__init__(root, width=size, height=size, **kw)
 
-        self.squares = [Square(self, i, size=size // 8) for i in range(64)]
+        self.squares = [Square(self, i, size=int(size) // 8) for i in range(64)]
+        self.root = root
+        self.board_size = int(size)
 
-        Piece(self.squares[0], "Q").pack()
+        piece_test = Piece(self, "Q")
+        x, y = self.squares[40].winfo_x(), self.squares[40].winfo_y()
+        piece_test.place(x=x, y=y)
 
 
 class MainWindow(tk.Tk):
@@ -49,7 +94,7 @@ class MainWindow(tk.Tk):
         self.title("Profchessor")
         # self.geometry("800x800")
         self.configure(bg=shared.BG_COLOR)
-        #self.wm_attributes("-fullscreen", True)  # Fullscreen
+        # self.wm_attributes("-fullscreen", True)  # Fullscreen
 
         self.board = Board(self, size=self.winfo_screenheight() * 0.8)
         self.board.pack(anchor="center", expand=True)
